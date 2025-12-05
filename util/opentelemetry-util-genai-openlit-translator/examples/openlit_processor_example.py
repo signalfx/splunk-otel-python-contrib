e8ebb428d44750ed97cfcebc2f8f450c37772a57
Pavan Sudheendra <pavan0591@gmail.com>
2025-12-05 13:37:42 +0000

    feat: initial commit of the openlit translator
    Signed-off-by: Pavan Sudheendra <pavan0591@gmail.com>

---
+++ util/opentelemetry-util-genai-openlit-translator/examples/openlit_processor_example.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from openai import OpenAI
+import openlit
+from dotenv import load_dotenv
+import os
+import traceback
+
+load_dotenv()
+
+try:
+    openlit.init(otlp_endpoint="http://0.0.0.0:4318")
+
+    client = OpenAI(
+        api_key=os.getenv("OPENAI_API_KEY")
+    )
+
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": "What is LLM Observability?",
+            }
+        ],
+        model="gpt-3.5-turbo",
+    )
+    print("response:", chat_completion.choices[0].message.content)
+except Exception as e:
+    print(f"An error occurred: {e}")
+    traceback.print_exc()
