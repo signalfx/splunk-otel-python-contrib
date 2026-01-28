e21348415dfdfdffe16c20b00f33553a497a4d18
Wrisa <ridhimasatam@gmail.com>
2026-01-28 14:26:47 -0800

    fixed test

--- instrumentation-genai/opentelemetry-instrumentation-langchain/src/opentelemetry/instrumentation/langchain/callback_handler.py
+++ instrumentation-genai/opentelemetry-instrumentation-langchain/src/opentelemetry/instrumentation/langchain/callback_handler.py
@@ -60,7 +60,7 @@ def _make_input_message(data: dict[str, Any]) -> list[InputMessage]:
         if content:
             # TODO: for invoke_agent type invocation, when system_messages is added, can filter SystemMessage separately if needed and only add here HumanMessage, currently all messages are added
             input_message = InputMessage(
-                role="user", parts=[Text(_safe_str(msg.content))]
+                role="user", parts=[Text(_safe_str(content))]
             )
             input_messages.append(input_message)
     return input_messages
